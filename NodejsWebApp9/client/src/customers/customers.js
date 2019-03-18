dbf3108cdd5e8c2368c5f4706d6086a23d878958
nicolasolo <41766950+nicolasolo@users.noreply.github.com>
2019-03-18 12:30:31 +0200

    2

---
+++ NodejsWebApp9/client/src/customers/customers.js
@@ -0,0 +1,42 @@
+﻿import React, { Component } from 'react';
+import './customers.css';
+
+class Customers extends Component {
+    constructor() {
+        super()
+        this.state = {
+            customers: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('api/customers')
+            .then(res => res.json())
+            .then(customers => this.setState({ customers }))
+    }
+
+
+    render() {
+        return (
+            <div>
+
+                <h1>Customers</h1>
+                <ul>
+                    {this.state.customers.map(customer =>
+                        <li key={customer.id}>{customer.firstName}  {customer.lastName}</li>
+                        
+                        )}
+
+                </ul>
+
+
+
+            </div>
+
+            )
+    }
+
+};
+
+
+export default Customers;
